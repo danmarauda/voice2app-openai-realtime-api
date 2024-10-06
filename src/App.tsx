564c6237539e6218a4f187effb04a227759ef602
Cameron King <cyking1233@gmail.com>
2024-10-06 02:41:01 +0000

    Generate Next pages using Voice

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { ConsolePage } from './pages/ConsolePage';
 import './App.scss';
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <div data-component="App">
+      <Analytics />
       <ConsolePage />
     </div>
   );
